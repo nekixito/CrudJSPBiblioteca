c7b7dddc26bca8941ba348b97ab14839b9218b35
Miguel Nequiz <nekixito@gmail.com>
2022-06-08 14:45:37 -0500

    Clase de conexión a la base de datos

---
+++ src/main/java/com/nekixitodev/crudjspbiblioteca/dao/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nekixitodev.crudjspbiblioteca.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Miguel Nequiz
+ */
+public class Conexion {
+    public static Connection conectar(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca", "root", "");
+        } catch (ClassNotFoundException ex) {
+            return null;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }
+}
